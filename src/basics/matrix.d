40a8bfe6032fe751015101f1ba504ebb5321c3df
SimonN <eiderdaus@gmail.com>
2015-12-01 17:41:37 +0100

    CTFE basher mask

--- src/basics/matrix.d
+++ src/basics/matrix.d
@@ -28,22 +28,10 @@ struct XY {
 
 class Matrix(T) {
 
-public:
-
-/*  this(in int _xl, in int _yl);
- *  this(in Matrix!T);
- *  ~this() { }
- *
- *  T    get(in int x, in int y) const;
- *  void set(in int x, in int y, in T value);
- *
- *  override string toString() const; -- exists, see below
- */
-
 private:
 
-    int xl;
-    int yl;
+    int _xl;
+    int _yl;
 
     T[] data;
 
@@ -51,75 +39,89 @@ private:
 
 public:
 
-this(
-    in int new_xl,
-    in int new_yl
-)
-in {
-    assert (new_xl > 0);
-    assert (new_yl > 0);
-}
-body {
-    xl = new_xl;
-    yl = new_yl;
-    data = new T[xl * yl];
-}
+    @property int xl() const { return _xl; }
+    @property int yl() const { return _yl; }
 
-
-
-this(Matrix!T rhs)
-out {
-    assert (data !is null);
-    assert (data.length == xl * yl);
-}
-body {
-    assert (rhs !is null);
-    xl = rhs.xl;
-    yl = rhs.yl;
-    data = rhs.data.dup;
-}
-
-
-
-inout(T) get(in int x, in int y) inout
-in {
-    assert (x >= 0);
-    assert (y >= 0);
-    assert (x < xl);
-    assert (y < yl);
-}
-body {
-    return data[y * xl + x];
-}
-
-
-
-void set(in int x, in int y, T value)
-in {
-    assert (x >= 0);
-    assert (y >= 0);
-    assert (x < xl);
-    assert (y < yl);
-}
-body {
-    data[y * xl + x] = value;
-}
-
-
-
-override string toString() const
-{
-    string s = "";
-    for (int y = 0; y < yl; ++y) {
-        s ~= "[ ";
-        for (int x = 0; x < xl; ++x) {
-            if (x > 0) s ~= ", ";
-            s ~= to!string(get(x, y));
-        }
-        s ~= " ]\n";
+    this(
+        in int new_xl,
+        in int new_yl
+    )
+    in {
+        assert (new_xl > 0);
+        assert (new_yl > 0);
     }
-    return s;
-}
+    body {
+        _xl = new_xl;
+        _yl = new_yl;
+        data = new T[_xl * _yl];
+    }
+
+
+
+    this(Matrix!T rhs)
+    out {
+        assert (data !is null);
+        assert (data.length == _xl * _yl);
+    }
+    body {
+        assert (rhs !is null);
+        _xl = rhs.xl;
+        _yl = rhs.yl;
+        data = rhs.data.dup;
+    }
+
+
+
+    inout(T) get(in int x, in int y) inout
+    in {
+        assert (x >= 0);
+        assert (y >= 0);
+        assert (x < _xl);
+        assert (y < _yl);
+    }
+    body {
+        return data[y * _xl + x];
+    }
+
+
+
+    void set(in int x, in int y, T value)
+    in {
+        assert (x >= 0);
+        assert (y >= 0);
+        assert (x < _xl);
+        assert (y < _yl);
+    }
+    body {
+        data[y * _xl + x] = value;
+    }
+
+
+
+    override bool opEquals(Object rhsObj) const
+    {
+        typeof(this) rhs = cast (typeof(this)) rhsObj;
+        assert (rhs, "can't compare against non-Matrix");
+        return _xl == rhs._xl
+            && _yl == rhs._yl
+            && data == rhs.data;
+    }
+
+
+
+    override string toString() const
+    {
+        string s = "";
+        for (int y = 0; y < _yl; ++y) {
+            s ~= "[ ";
+            for (int x = 0; x < _xl; ++x) {
+                if (x > 0) s ~= ", ";
+                s ~= to!string(get(x, y));
+            }
+            s ~= " ]\n";
+        }
+        return s;
+    }
 
 }
-// end class
+// end class Matrix(T)
---
+++ src/graphic/mask.d
@@ -0,0 +1,141 @@
+module graphic.mask;
+
+import std.conv;
+import std.algorithm;
+import std.range;
+import std.string;
+
+import basics.help;
+import basics.matrix;
+
+enum bashLeft  = bashRight.mirrored;
+enum bashRight = Mask([
+    "XXXXXXXX" "XXXX....", // Top 2 rows can cut through steel without
+    "XXXXXXXX" "XXXXXX..", // cancelling the basher. Other rows would cancel.
+    "XXXXXXXX" "XXXXXXX."] ~
+    "XXXXXXXX" "XXXXXXXX".repeat(12).array ~ [
+    "XXXXXXXX" "XXXXXXX.",
+    "#XXXXXXX" "XXXXXX..", // '#' = effective coordinate
+    "XXXXXXXX" "XXXX....",
+]);
+
+enum bashNoRelicsLeft  = bashRight.mirrored;
+enum bashNoRelicsRight = Mask(
+    "XXXXXXXX" "XXXXXXXX".repeat(16).array ~ [
+    "#XXXXXXX" "XXXXXXXX", // '#' = effective coordinate
+    "XXXXXXXX" "XXXXXXXX",
+]);
+
+enum mineLeft  = mineRight.mirrored;
+enum mineRight = Mask([
+    "...XXXXX" "........" "..", // -20
+    ".XXXXXXX" "XX......" "..",
+    "XXXXXXXX" "XXXX...." "..",
+    "XXXXXXXX" "XXXXXX.." "..",
+    "XXXXXXXX" "XXXXXXX." "..", // -16
+    "XXXXXXXX" "XXXXXXXX" "..", // -15
+    "XXXXXXXX" "XXXXXXXX" "X.", // -14
+    "XXXXXXXX" "XXXXXXXX" "X.", ] ~
+    "XXXXXXXX" "XXXXXXXX" "XX".repeat(12).array ~ [ // from -12 to -1 inclusive
+    "#XXXXXXX" "XXXXXXXX" "X.", // 0, with '#' the effective coordinate
+    "XXXXXXXX" "XXXXXXXX" "X.", // 1
+    "..XXXXXX" "XXXXXXXX" "..", // 2 = old ground level
+    "....XXXX" "XXXXXXX." "..", // 3
+    "......XX" "XXXXXX.." "..", // 4
+    "........" "XXXX...." "..", // 5 = deepest air = miner hole depth is 4 px
+]);
+
+struct Mask {
+
+    enum CharOK : char {
+        solid       = 'X',
+        air         = '.',
+        solidOffset = '#',
+        airOffset   = 'o'
+    }
+
+    int offsetX;
+    int offsetY;
+
+    private Matrix!bool _mat;
+    const(Matrix!bool) aliasThis() const { return _mat; }
+    alias aliasThis this;
+
+    this(in string[] strs)
+    in {
+        assert (strs.length    > 0, "need at least 1 row");
+        assert (strs[0].length > 0, "need at least 1 column");
+        assert (strs.all!(a => a.length == strs[0].length),
+            "matrix of chars is not rectangular");
+    }
+    body {
+        _mat = new Matrix!bool(strs[0].len, strs.len);
+        bool offsetSet = false;
+        foreach     (const int y, const string s; strs)
+            foreach (const int x, const char   c; s) {
+                CharOK cc;
+                try cc = std.conv.to!CharOK(c);
+                catch (Exception)
+                    assert (false, format(
+                        "Bad character in string %d, `%s', "
+                        "at position %d: `%c'. "
+                        "Expected `.', `o', `X', or `#'.",
+                        y, s, x, c).idup);
+                _mat.set(x, y, cc == CharOK.solid
+                            || cc == CharOK.solidOffset);
+                if (   cc == CharOK.solidOffset
+                    || cc == CharOK.airOffset
+                ) {
+                    assert (! offsetSet, format(
+                        "Offset is (%d, %d), but there is another "
+                        "offset-setting char `%c' at (%d, %d).",
+                        offsetX, offsetY, c, x, y).idup);
+                    offsetX = x;
+                    offsetY = y;
+                    offsetSet = true;
+                }
+            }
+    }
+    // end this()
+
+    Mask mirrored() const
+    in {
+        assert (xl      % 2 == 0, "can't mirror a matrix with odd xl");
+        assert (offsetX % 2 == 0, "can't mirror a matrix with odd offsetX");
+    }
+    body {
+        Mask ret;
+        ret._mat = new Matrix!bool(xl, yl);
+        foreach     (const int y; 0 .. ret._mat.yl)
+            foreach (const int x; 0 .. ret._mat.xl)
+                ret._mat.set(x, y, this.get(xl - 1 - x, y));
+        // Enforce the offset at an even coordinate, because the physics use
+        // 2-pixel-wide chunks everywhere, using the left pixel's coordinates.
+        ret.offsetX = basics.help.even(xl - 1 - offsetX);
+        ret.offsetY = offsetY;
+        return ret;
+    }
+
+}
+
+unittest {
+    Mask a = Mask([
+        ".X.X..",
+        "..oX..",
+        ".XX...",
+    ]);
+    assert (  a.get(3, 0));
+    assert (  a.get(3, 1));
+    assert (! a.get(0, 0));
+    assert (! a.get(2, 1));
+    assert (a.offsetX == 2);
+    assert (a.offsetY == 1);
+
+    Mask b = Mask([
+        "..X.X.",
+        "..#...",
+        "...XX.",
+    ]);
+    assert (b.offsetX == 2);
+    assert (b == a.mirroredWithEvenOffsetX());
+}
