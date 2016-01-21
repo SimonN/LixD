2562290f02cfde3b2e34cf77f0bd38f55dacee38
SimonN <eiderdaus@gmail.com>
2016-01-21 06:48:32 +0100

    panel bar has icons for in, out, etc.

--- src/game/gui/panelinf.d
+++ src/game/gui/panelinf.d
@@ -3,8 +3,10 @@ module game.gui.panelinf;
 import std.string; // format;
 import std.typecons; // Rebindable;
 
+import basics.globals; // game panel icons
 import basics.user; // languageIsEnglish
 import game.tribe;
+import graphic.gralib;
 import gui;
 import lix;
 
@@ -23,11 +25,7 @@ class PanelStats : Button {
     this(Geom g)
     {
         super(g);
-        _targetDesc = new Label(new Geom(
-            TextButton.textXFromLeft, 0, this.xlg, this.ylg, From.RIGHT));
-        _debugging = new Label(new Geom(
-            TextButton.textXFromLeft, 0, this.xlg, this.ylg));
-        addChildren(_targetDesc, _debugging);
+        implConstructor();
     }
 
     @property int
@@ -55,16 +53,31 @@ class PanelStats : Button {
     void suggestTooltipBuilders() { }
     void suggestTooltipPlatformers() { }
 
-    void showTribe(in Tribe tribe) {
-        with (tribe)
+    void showTribe(in Tribe tribe) { with (tribe)
     {
+        assert (tribe);
         reqDraw();
-        if (tribe !is null)
-            _debugging.text = "out %d/%d, in %d/%d"
-                .format(lixHatch + lixOut, lixInitial, lixSaved, lixRequired);
-        else
-            _debugging.text = "tribeLocal is null";
+        _lOut  .number = lixHatch + lixOut;
+        _lHatch.number = lixHatch;
+        _lSaved.text   = "%d/%d".format(lixSaved, lixRequired);
+        _lTime .text   = "(---)";
+        _bOut.yf   = lixHatch + lixOut > 0 ? 0 : 1;
+        _bHatch.yf = 1;
+        _bSaved.yf = lixSaved == 0 ? 1 : lixSaved >= lixRequired ? 2 : 0;
+        _bTime.yf  = 1;
     }}
+
+protected:
+
+    override void calcSelf()
+    {
+        down = false;
+    }
+
+    override void drawOntoButton()
+    {
+        formatTargetDesc();
+    }
 
 private:
 
@@ -72,8 +85,8 @@ private:
     Rebindable!(const(Lixxie)) _targetDescLixxie;
     Rebindable!(const(Tribe))  _tribe;
 
-    Label _targetDesc;
-    Label _debugging; // split into many labels
+    CutbitElement _bOut, _bHatch, _bSaved, _bTime;
+    Label         _lOut, _lHatch, _lSaved, _lTime, _targetDesc;
 
     void formatTargetDesc()
     in {
@@ -91,15 +104,25 @@ private:
             _targetDescNumber > 1 && languageIsEnglish ? "s" : "");
     }
 
-protected:
-
-    override void calcSelf()
+    void implConstructor()
     {
-        down = false;
-    }
-
-    override void drawOntoButton()
-    {
-        formatTargetDesc();
+        assert (! _bOut);
+        auto makeElements(ref CutbitElement cbe, ref Label lab,
+            in int x, in int xl, in int xf
+        ) {
+            cbe = new CutbitElement(new Geom(x, 0, this.ylg, this.ylg,
+                            From.LEFT), getInternal(fileImageGameIcon));
+            cbe.xf = xf;
+            lab = new Label(new Geom(x + this.ylg, 0, xl - this.ylg, this.ylg,
+                            From.LEFT));
+            addChildren(cbe, lab);
+        }
+        makeElements(_bOut,   _lOut,     0, 60, 3);
+        makeElements(_bHatch, _lHatch,  60, 60, 4);
+        makeElements(_bSaved, _lSaved, 120, 80, 5);
+        makeElements(_bTime,  _lTime,  200, 70, 7);
+        _targetDesc = new Label(new Geom(
+            TextButton.textXFromLeft, 0, this.xlg, this.ylg, From.RIGHT));
+        addChild(_targetDesc);
     }
 }
