cbb3250fd2d486636d8941658196339a6a69f661
SimonN <eiderdaus@gmail.com>
2016-03-02 20:55:41 +0100

    editor annotates hatches and goals

--- src/editor/draw.d
+++ src/editor/draw.d
@@ -1,12 +1,14 @@
 module editor.draw;
 
 import std.algorithm;
+import std.string;
 
 import basics.alleg5;
 import editor.editor;
 import editor.hover;
 import graphic.color;
 import graphic.cutbit;
+import graphic.textout;
 import hardware.display;
 import hardware.tharsis;
 import tile.gadtile;
@@ -43,6 +45,7 @@ void drawMainMap(Editor editor) {
     editor._map.clearScreenRect(color.makecol(bgRed, bgGreen, bgBlue));
     editor.drawGadgets();
     editor._map.loadCameraRect(editor._mapTerrain);
+    editor.drawGadgetAnnotations();
     editor.drawHovers(_hover, false);
     editor.drawHovers(_selection, true);
 }}
@@ -55,6 +58,21 @@ void drawGadgets(Editor editor)
             assert (g.ob && g.ob.cb);
             g.ob.cb.draw(editor._map, g.x, g.y);
         }
+}
+
+void drawGadgetAnnotations(Editor editor)
+{
+    auto zone = Zone(profiler, "Editor.drawGadgetAnnotations");
+    void annotate(const(typeof(editor._level.pos[0])) list)
+    {
+        foreach (int i, g; list) {
+            assert (g.ob && g.ob.cb);
+            string s = "%d/%d".format(i+1, list.length);
+            drawTextCentered(djvuM, s, g.x + g.ob.cb.xl/2, g.y, color.guiText);
+        }
+    }
+    annotate(editor._level.pos[GadType.HATCH]);
+    annotate(editor._level.pos[GadType.GOAL]);
 }
 
 void drawHovers(Editor editor, const(Hover[]) list, in bool light)
