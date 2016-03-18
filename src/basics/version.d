9ff5de5906d12bfe6b074e0fca0d7885db952411
SimonN <eiderdaus@gmail.com>
2016-03-18 17:41:12 +0100

    version to 0.2.41

--- src/basics/version.d
+++ src/basics/version.d
@@ -9,7 +9,7 @@ module basics.versioning;
 import std.conv;
 import std.string;
 
-private immutable _gameVersion     = Version(0, 2, 40);
+private immutable _gameVersion     = Version(0, 2, 41);
 private bool      _versionIsStable = false;
 
 const(Version) gameVersion()     { return _gameVersion;     }
