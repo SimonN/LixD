a64de438af29e8736ccdd6abe268233834273601
SimonN <eiderdaus@gmail.com>
2017-08-21 03:04:19 +0200

    tumbler initializes pixelsFallen by simul

--- src/lix/skill/tumbler.d
+++ src/lix/skill/tumbler.d
@@ -39,17 +39,11 @@ abstract class BallisticFlyer : Job {
         assert (speedX >= 0, "Lix should only fly forwards. Turn first.");
         if (speedX % 2 != 0)
             ++speedX;
-
         moveSeveralTimes();
-
         if (this !is lixxie.job)
             return;
-
-        if      (speedY <= 12) speedY += 2;
-        else if (speedY  < 32) speedY += 1;
-
+        speedY += accel(speedY);
         selectFrame();
-
         if (speedY >= speedYToFloat) {
             if      (abilityToFloat)  become(Ac.floater);
             else if (ac == Ac.jumper) become(Ac.tumbler);
@@ -60,6 +54,12 @@ protected:
 
     final bool wall  (in int y) { return isSolid(0, y); }
     final bool behind(in int y) { return wall(y) && isSolid(-2, y); }
+
+    enum int biggestLargeAccelSpeedY = 12;
+    final static int accel(int ysp) pure
+    {
+        return (ysp <= biggestLargeAccelSpeedY) ? 2 : (ysp < 32) ? 1 : 0;
+    }
 
     bool splatUpsideDown() const { return false; }
 
@@ -319,7 +319,7 @@ class Tumbler : BallisticFlyer {
             assert (tumbling);
             tumbling.speedX = wantFlingX.abs;
             tumbling.speedY = wantFlingY;
-            tumbling.pixelsFallen = 0; // you can break falls by flinging
+            tumbling.initPixelsFallen();
             tumbling.selectFrame();
         }
         else
@@ -397,4 +397,55 @@ protected:
             + ((res.targetFrame == frame && res.anim) ? 1 : 0);
     }
 
+private:
+    void initPixelsFallen()
+    {
+        pixelsFallen = 0;
+        if (speedY < 0)
+            return;
+        // In the check, ysp < speedY is correct, not ysp <= speedY.
+        // Even if we begin with speedY == 2, we have fallen 0 pixels!
+        // This is because we initialize with a speed, then use that speed
+        // on the next physics update, only then increase the speed.
+        // Thus, if we initialize speed with 2, we will fly 2 pixels down
+        // on the next update and set speed to 4. Compared to speedY == 0,
+        // we didn't fall more pixels before reaching speed 4, we merely
+        // reached it sooner: The (speed == 0)-tumbler waited before falling.
+        for (int ysp = speedY <= biggestLargeAccelSpeedY + 1 ? speedY % 2 : 0;
+            ysp < speedY && accel(ysp) > 0;
+            ysp += accel(ysp)
+        ) {
+            pixelsFallen += ysp;
+        }
+    }
+}
+
+unittest {
+    auto t = new Tumbler();
+    int wouldHaveFallen(int speed)
+    {
+        t.speedY = speed;
+        t.initPixelsFallen();
+        return t.pixelsFallen;
+    }
+    assert (wouldHaveFallen(0) == 0);
+    assert (wouldHaveFallen(2) == 0);
+    assert (wouldHaveFallen(4) == 2);
+    assert (wouldHaveFallen(6) == 6);
+    assert (wouldHaveFallen(8) == 12);
+    assert (wouldHaveFallen(10) == 20);
+    assert (wouldHaveFallen(12) == 30); // 12 is biggest speedY with accel == 2
+    assert (wouldHaveFallen(14) == 42);
+    assert (wouldHaveFallen(15) == 56);
+    assert (wouldHaveFallen(16) == 71);
+    assert (wouldHaveFallen(31) != wouldHaveFallen(32));
+    assert (wouldHaveFallen(32) == wouldHaveFallen(33));
+    assert (wouldHaveFallen(60) == wouldHaveFallen(80));
+
+    assert (wouldHaveFallen(1) == 0);
+    assert (wouldHaveFallen(3) == 1);
+    assert (wouldHaveFallen(13) == 36); // in between values for 12 and 14
+
+    for (int speed = 2; speed < 32; ++speed)
+        assert (wouldHaveFallen(speed) < wouldHaveFallen(speed + 1));
 }
