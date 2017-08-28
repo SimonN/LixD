4e4751a6d7c1bf2f5bfe44b66fc0528f423958f5
SimonN <eiderdaus@gmail.com>
2017-08-28 06:51:34 +0200

    0.8.4

--- src/net/version.d
+++ src/net/version.d
@@ -10,7 +10,7 @@ import std.bitmanip;
 import std.conv;
 import std.string;
 
-private immutable _gameVersion = Version(0, 8, 3);
+private immutable _gameVersion = Version(0, 8, 4);
 const(Version) gameVersion() { return _gameVersion; }
 
 struct Version {
