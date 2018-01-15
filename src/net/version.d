8578d9921a5480dfbc1af8b428f2f5778d691444
Simon Naarmann <s.naarmann@gmail.com>
2018-01-15 19:12:16 +0100

    0.9.9

--- src/net/version.d
+++ src/net/version.d
@@ -10,7 +10,7 @@ import std.bitmanip;
 import std.conv;
 import std.string;
 
-private immutable _gameVersion = Version(0, 9, 8);
+private immutable _gameVersion = Version(0, 9, 9);
 const(Version) gameVersion() { return _gameVersion; }
 
 struct Version {
