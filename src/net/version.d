7cac78c5f77b7cfc3b7dea373db6226376e730f9
Simon Naarmann <s.naarmann@gmail.com>
2021-02-21 21:16:32 +0100

    0.9.36

--- src/net/version.d
+++ src/net/version.d
@@ -10,7 +10,7 @@ import std.bitmanip;
 import std.conv;
 import std.string;
 
-private immutable _gameVersion = Version(0, 9, 35);
+private immutable _gameVersion = Version(0, 9, 36);
 const(Version) gameVersion() { return _gameVersion; }
 
 struct Version {
