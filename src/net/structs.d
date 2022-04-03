SimonN/LixD

commit 7f36fc25168c24ca10e16a8fb8d7914e7e7959ec

Fix ChatPacket length: Don't send more than maxLen
Add unittest for ChatPacket length.
This is green now, would have been red before.

src/net/structs.d
@@ -247,8 +247,9 @@ struct ChatPacket {
     static assert (netChatMaxLen <= 0xFFFF);
     int len() const pure nothrow @safe @nogc
     {
-        return header.len + 1
-            + (max(netChatMaxLen & 0xFFFF, text.length & 0xFFFF) & 0xFFFF);
+        return header.len
+            + (min(netChatMaxLen & 0xFFFF, text.length & 0xFFFF) & 0xFFFF)
+            + 1; // Terminating nullbyte
     }
 
     ENetPacket* createPacket() const nothrow
@@ -268,6 +269,25 @@ struct ChatPacket {
         if (p.data[p.dataLength - 1] == '\0')
             text = fromStringz(cast (char*) (p.data + header.len)).idup;
     }
+}
+
+unittest {
+    import net.enetglob;
+    initializeEnet();
+    scope (exit)
+        deinitializeEnet();
+
+    ChatPacket chat;
+    chat.text = "Hello";
+    assert (chat.len == 2 + 5 + 1);
+
+    auto p = chat.createPacket();
+    assert (p.dataLength == chat.len);
+
+    const decoded = ChatPacket(p);
+    enet_packet_destroy(p);
+    assert (decoded.text == chat.text);
+    assert (decoded.len == chat.len);
 }
 
 struct MillisecondsSinceGameStartPacket {

commit e3370efc65cde4cc24ded30f32dffcfb16db1924

Fix memory corruption crash on sending chat
Memory corruption introduced in commit:
7f36fc25168c24ca10e16a8fb8d7914e7e7959ec

I made len() return fewer bytes, but didn't fix the accompanying
strncpy() call to adhere to the shorter len().
The unstable server code that handles both 0.9 and the future 0.10 won't
call strncpy() anymore, but that code is too big, therefore, here's a
new fix that applies much more easily to the 0.9.43 code.

src/net/structs.d
@@ -257,7 +257,7 @@ struct ChatPacket {
         auto ret = .createPacket(len);
         header.serializeTo(ret.data[0 .. header.len]);
         strncpy(cast (char*) (ret.data + header.len), text.toStringz,
-                                                      netChatMaxLen);
+            len - header.len);
         ret.data[len - 1] = '\0';
         return ret;
     }