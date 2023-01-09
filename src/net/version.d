f02fdc7216d8cc58504bce8c545bf5b185c763d3
Simon Naarmann <s.naarmann@gmail.com>
2023-01-09 19:43:42 +0100

    0.10.4

--- src/net/version.d
+++ src/net/version.d
@@ -12,7 +12,7 @@ import std.conv;
 import std.range;
 import std.string;
 
-private immutable _gameVersion = Version(0, 10, 3);
+private immutable _gameVersion = Version(0, 10, 4);
 const(Version) gameVersion() { return _gameVersion; }
 
 struct Version {
