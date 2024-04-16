d3f2970dcc842de4f9ebb09bacd3ebf467dffce4
Simon Naarmann <s.naarmann@gmail.com>
2024-04-16 16:21:19 +0200

    Windows batch: Prefer LDC, but allow DMD
    Tested in Wine (as-is), in cmd (as earlier variant) and in PowerShell
    (as an earlier variant, and PowerShell calls cmd to run the script).

--- win-build.bat
+++ win-build.bat
@@ -1,2 +1,20 @@
-dub -v build --build=release --compiler=ldc2 --arch=x86_64
+@echo off
+
+rem Build script for Windows or Wine. We prefer LDC here because LDC builds
+rem the fastest-running Lix binaries, but you can use any D compiler.
+
+echo Looking for an installed LDC ...
+where.exe ldc2
+if %errorlevel%==0 (
+    echo Building Lix with LDC ...
+    dub build --build=release --arch=x86_64 --compiler=ldc2
+) else (
+    echo Building Lix with any one compiler ...
+    dub build --build=release --arch=x86_64
+)
+
+if %errorlevel%==0 (
+    echo Look in the folder "bin" for the Lix executable.
+)
+
 pause
